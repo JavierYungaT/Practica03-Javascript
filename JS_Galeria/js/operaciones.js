5448acc7da9cec2357870fff393be4854d0e3964
JavierYungaT <49315481+JavierYungaT@users.noreply.github.com>
2019-05-22 13:29:03 -0500

    Modificacion

--- JS_Galeria/js/operaciones.js
+++ JS_Galeria/js/operaciones.js
@@ -10,6 +10,7 @@ function puntos(n){
     }
     ptn[n].className += " activo";
 }
+
 function mostrar(n){
     var imagenes = document.getElementsByClassName("imagen");
     for(i = 0; i< imagenes.length; i++){
@@ -29,6 +30,20 @@ function siguiente(){
         actual = 0;
     }
     mostrar(actual);
+    var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n == x.length)
+		document.getElementById('right').disabled = true;
+	else
+		document.getElementById('right').disabled = false;
+
+	if (n == 1) 
+		document.getElementById('left').disabled = true;
+	else
+		document.getElementById('left').disabled = false
+
+	for (i = 0; i < x.length; i++)
+		x[i].style.display = "none";
 }
 function anterior(){
     actual--;
@@ -38,6 +53,7 @@ function anterior(){
     mostrar(actual);
 }
 
+/*
 var velocidad = 2000;
 var play = setInterval("siguiente()", velocidad);
 
@@ -51,4 +67,52 @@ function playpause(){
         play = null;
         boton.src = "images/play.png";
     }
+}
+*/
+var slideIndex = 1;
+showDivs(slideIndex);
+
+window.onload = function() {
+	iniciar();
+}
+
+function iniciar() {
+	slideIndex = 1;
+	showDivs(1);
+	array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	var val;
+	for(i = 0; i < 5; i++) 
+		array.splice(Math.floor(Math.random() * array.length), 1);
+
+	shuffle(array);
+
+	for(i = 0; i < 5; i++) 
+		document.getElementById(i).src = "img/"+ array[i] +".jpg"+".png";
+}
+
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n == x.length)
+		document.getElementById('right').disabled = true;
+	else
+		document.getElementById('right').disabled = false;
+
+	if (n == 1) 
+		document.getElementById('left').disabled = true;
+	else
+		document.getElementById('left').disabled = false
+
+	for (i = 0; i < x.length; i++)
+		x[i].style.display = "none";
+
+	x[slideIndex-1].style.display = "block"; 
+}
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
 }
