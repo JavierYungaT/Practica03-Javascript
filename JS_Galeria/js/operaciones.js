8adb93975c2c2740f549bc5f295c379ef5036225
JavierYungaT <49315481+JavierYungaT@users.noreply.github.com>
2019-05-23 02:53:54 -0500

    Modificacion , actualizacion y eliminacion

--- JS_Galeria/js/operaciones.js
+++ JS_Galeria/js/operaciones.js
@@ -1,74 +1,3 @@
-
-var actual = 0;
-function puntos(n){
-    var ptn = document.getElementsByClassName("punto");
-    for(i = 0; i<ptn.length; i++){
-        if(ptn[i].className.includes("activo")){
-            ptn[i].className = ptn[i].className.replace("activo", "");
-            break;
-        }
-    }
-    ptn[n].className += " activo";
-}
-
-function mostrar(n){
-    var imagenes = document.getElementsByClassName("imagen");
-    for(i = 0; i< imagenes.length; i++){
-        if(imagenes[i].className.includes("actual")){
-            imagenes[i].className = imagenes[i].className.replace("actual", "");
-            break;
-        }
-    }
-    actual = n;
-    imagenes[n].className += " actual";
-    puntos(n);
-}
-
-function siguiente(){
-    actual++;
-    if(actual > 4){
-        actual = 0;
-    }
-    mostrar(actual);
-    var i;
-	var x = document.getElementsByClassName("mySlides");
-	if (n == x.length)
-		document.getElementById('right').disabled = true;
-	else
-		document.getElementById('right').disabled = false;
-
-	if (n == 1) 
-		document.getElementById('left').disabled = true;
-	else
-		document.getElementById('left').disabled = false
-
-	for (i = 0; i < x.length; i++)
-		x[i].style.display = "none";
-}
-function anterior(){
-    actual--;
-    if(actual < 0){
-        actual = 4;
-    }
-    mostrar(actual);
-}
-
-/*
-var velocidad = 2000;
-var play = setInterval("siguiente()", velocidad);
-
-function playpause(){
-    var boton = document.getElementById("btn");
-    if(play == null){
-        boton.src = "images/pause.png";
-        play = setInterval("siguiente()", velocidad);
-    } else {
-        clearInterval(play);
-        play = null;
-        boton.src = "images/play.png";
-    }
-}
-*/
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -87,7 +16,7 @@ function iniciar() {
 	shuffle(array);
 
 	for(i = 0; i < 5; i++) 
-		document.getElementById(i).src = "img/"+ array[i] +".jpg"+".png";
+		document.getElementById(i).src = "img/"+ array[i] +".jpg";
 }
 
 function plusDivs(n) {
